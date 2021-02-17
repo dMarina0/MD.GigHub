95144c9eb9159c26933bf53a30698b8b39b87bbb
Marina Doci <d.maryna17@yahoo.com>
2021-02-17 21:50:19 +0200

    CSRF

--- MD.GigHub/Controllers/GigsController.cs
+++ MD.GigHub/Controllers/GigsController.cs
@@ -28,6 +28,7 @@ namespace MD.GigHub.Controllers
 
         [Authorize]
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Create(GigFormViewModel viewModel)
         {
             if (!ModelState.IsValid)
