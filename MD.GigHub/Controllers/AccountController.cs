929544d189404796e17159343fc831101a24d62b
Marina Doci <d.maryna17@yahoo.com>
2021-02-20 14:48:56 +0200

    update sign-up form

--- MD.GigHub/Controllers/AccountController.cs
+++ MD.GigHub/Controllers/AccountController.cs
@@ -151,7 +151,11 @@ namespace MD.GigHub.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    Name = model.Name
+                };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
